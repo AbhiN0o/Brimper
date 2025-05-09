2bbd87ac5418fc9706f5d2a650d367a3922ae0c9
Abhinav <dhruvsahu550@gmail.com>
2025-05-10 01:52:51 +0530

    Setup send friend request endpoint

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/User.js";
 
 export const getRecommendedUsers = async(req,res) => {
@@ -26,6 +27,56 @@ export const getMyfriends = async (req,res) => {
         res.status(200).json(user.friends)
     } catch (error) {
         console.error("Error in getMyFriends Controller",error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const sendFriendRequest = async ()=>{
+    try {
+        const myId=req.user._id
+        const recipientId =req.params.id
+
+        if(myId===recipientId){
+            return res.status(400).json({message:"You can't send friend request to yourself"})
+        }
+
+        const recipient= await User.findById(recipientId)
+        if(!recipient){
+            return res.status(400).json({message:"Recipient not found"})
+        }
+
+        if(recipient.friends.includes(myId)){
+            return res.status(400).json({message:"You are already friends with this user"})
+        }
+
+        //check if a request already exists
+
+        const existingRequest = await FriendRequest.findOne({
+            $or:[
+                {
+                    senderId:myId,
+                    recipient:recipientId
+                },
+                {
+                    sender:recipientId,
+                    recipient:myId
+                }
+            ]
+        })
+        
+        if(existingRequest){
+            return res.status(400).json({message:"A friend request already exists between you and this user"})
+        }
+
+        const friendRequest=await FriendRequest.create({
+            sender:myId,
+            recipient:recipientId,
+        })
+
+        res.status(201).json(friendRequest)
+
+    } catch (error) {
+        console.error("Error in sendFriendRequest controller",error);
         res.status(500).json({message:"Internal Server Error"})
     }
 }
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middleware.js"
-import { getMyfriends, getRecommendedUsers } from "../controllers/user.controller.js";
+import { getMyfriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 
 const router = express.Router()
@@ -13,5 +13,6 @@ router.get("/",protectRoute,getRecommendedUsers);
 
 router.get("/friends",protectRoute,getMyfriends);
 
+router.post("/friend-request/:id",sendFriendRequest)
 
 export default router
