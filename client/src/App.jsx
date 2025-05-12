8c3730f8dc3d7191f3168fa65b8d4ee1864ab1b4
Abhinav <dhruvsahu550@gmail.com>
2025-05-12 11:42:42 +0530

    Setup routes, pages and toast for notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-
+import { Route, Routes } from "react-router"
+import Homepage from "./pages/HomePage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SignupPage from "./pages/SignupPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import toast, {Toaster} from "react-hot-toast"
 function App() {
   
   return (
     <div className="h-screen">
-      hello
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/notifications" element={<NotificationsPage />}></Route>
+        <Route path="/call" element={<CallPage />}></Route>
+        <Route path="/chat" element={<ChatPage />}></Route>
+        <Route path="/onboarding" element={<OnboardingPage />}></Route>
+      </Routes>
+      <Toaster />
     </div>
   )
 }
---
+++ client/src/pages/CallPage.jsx
@@ -0,0 +1,9 @@
+
+export default function CallPage () {
+  return (
+    <div>
+      Call
+    </div>
+  )
+}
+
